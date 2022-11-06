a50f72c3e9659a1eca5ff996494dd38b40429bd5
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2022-11-06 23:06:50 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,8 +4,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-const playBackTime = "videoplayer-current-time"
+const lokalStorage = "videoplayer-current-time";
+// const saveCurrentTime =
 
-player.on('timeupdate', throttle((function (data) {
-    localStorage.setItem('playBackTime', JSON.stringify(data.seconds));
-}), 1000));
+// player.on('timeupdate', throttle(saveCurrentTime, 1000));
+player.on('timeupdate', throttle(evt => {
+    localStorage.setItem('videoplayer-current-time', evt.seconds)
+}, 1000)
+);
+
+// const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY)
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    .catch(function (error) {
+
+    })
+
+
+
