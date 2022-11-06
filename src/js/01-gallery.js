5dc80f779f0d836656a5ea9ac65e1adc9f3566c1
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2022-11-06 20:42:22 +0300

    summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/01-gallery.css';
+import '../css/common.css'
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+const imgGalleryEl = document.querySelector('.gallery');
+const markupGallery = createMarkUp(galleryItems);
+
+imgGalleryEl.innerHTML = markupGallery;
+
+function createMarkUp(galleryItems) {
+    return galleryItems
+        .map((item) => {
+            return `<div class ="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" 
+        src="${item.preview}"
+        alt="${item.description}"/>
+        </a>
+        </div>`
+        }).join("");
+};
+
+
+const simplelightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const playBackTime = "videoplayer-current-time"
+
+player.on('timeupdate', throttle((function (data) {
+    localStorage.setItem('playBackTime', JSON.stringify(data.seconds));
+}), 1000));
